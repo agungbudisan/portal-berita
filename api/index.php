330f9d1cd4f2120b022ddb9493da2e9efc1564c2
agungbudisan <agungbudisantoso@student.uns.ac.id>
2025-05-20 12:11:58 +0700

    Refactor admin and index API files for improved readability and consistency; update Vercel configuration to use routes instead of rewrites.

--- api/index.php
+++ api/index.php
@@ -1,7 +1,15 @@
 <?php
+// /api/index.php
+use Illuminate\Http\Request;
 
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Illuminate\Http\Request::capture());
+$response = $kernel->handle(
+    $request = Request::capture()
+);
 $response->send();
+$kernel->terminate($request, $response);
